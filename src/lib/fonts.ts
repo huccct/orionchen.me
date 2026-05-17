d440b7e64a425ba23c544abe54e9e3dde140b762
huccct <ucccth@gmail.com>
2026-05-17 20:38:34 +0800

    feat(visual): wire geist + newsreader + noto serif sc fonts

---
+++ src/lib/fonts.ts
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono, Newsreader, Noto_Serif_SC } from 'next/font/google'
+
+export const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+  display: 'swap',
+})
+
+export const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
+})
+
+export const newsreader = Newsreader({
+  subsets: ['latin'],
+  variable: '--font-newsreader',
+  weight: ['400', '600'],
+  display: 'swap',
+})
+
+export const notoSerifSC = Noto_Serif_SC({
+  subsets: ['latin'],
+  variable: '--font-noto-serif-sc',
+  weight: ['400', '700'],
+  display: 'swap',
+})
