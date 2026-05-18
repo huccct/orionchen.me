9935abde94f6a56f473db2dd7d346a2494fa8e68
huccct <ucccth@gmail.com>
2026-05-18 12:59:07 +0800

    feat(seo): rss feed at /rss.xml

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,48 @@
+import { allPosts } from 'content-collections'
+import { siteConfig } from '@/lib/site-config'
+
+export const dynamic = 'force-static'
+
+function cdata(value: string) {
+  return `<![CDATA[${value.replaceAll(']]>', ']]]]><![CDATA[>')}]]>`
+}
+
+function xmlEscape(value: string) {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+}
+
+export function GET() {
+  const posts = allPosts
+    .filter((post) => !post.draft)
+    .sort((a, b) => b.date.localeCompare(a.date))
+    .slice(0, 50)
+
+  const items = posts
+    .map((post) => {
+      const url = `${siteConfig.url}/blog/${post.slug}`
+
+      return `
+    <item>
+      <title>${cdata(post.title)}</title>
+      <link>${xmlEscape(url)}</link>
+      <guid>${xmlEscape(url)}</guid>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <description>${cdata(post.summary ?? '')}</description>
+    </item>`
+    })
+    .join('')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0"><channel>
+  <title>${xmlEscape(siteConfig.name)}</title>
+  <link>${xmlEscape(siteConfig.url)}</link>
+  <description>${xmlEscape(siteConfig.description)}</description>
+  ${items}
+</channel></rss>`
+
+  return new Response(xml, { headers: { 'Content-Type': 'application/xml' } })
+}
